666652cab99be789016e58e90dea0fe1223bcac7
fersan <fst-96@hotmail.com>
2021-01-29 16:55:55 -0600

    Agregando todos los archivos del webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+import {saludar } from "./js/componentes.js";
+// import img from './assets/maxresdefault.jpg';
+// Esto funcionaba con la version 5 ahora con la version 6 es diferente
+
+
+saludar('Fernando');
